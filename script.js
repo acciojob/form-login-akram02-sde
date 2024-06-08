f0a9766969b28abba10c6ecd538575da7a6a7bb0
Akram Ahmed Khan <68415682+akram02-sde@users.noreply.github.com>
2024-06-08 15:38:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+let fname = document.querySelectorAll('input')[0];
+let lname = document.querySelectorAll('input')[1];
 function getFormvalue() {
     //Write your code here
+	// e.preventDefault();
+	alert(fname.value + " " + lname.value)
 
 }
